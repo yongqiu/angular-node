20b3239d4e9f941ab2673c177dfbf308da14efd8
wuyongqiu <416733052@qq.com>
2018-10-26 00:54:00 +0800

    no message

--- node/routers/api_robot.js
+++ node/routers/api_robot.js
@@ -5,7 +5,7 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 // 表
-var r_user = require('../sql/r_user');
+var { r_role, r_user } = require('../sql/r_user');
 
 // 数据库
 var dbConfig = require('../config/dbconfig');
@@ -15,20 +15,32 @@ var pool = mysql.createPool(dbConfig.mysql);
 
 
 
-router.post('/authorData', function (req, res, next) {
+router.post('/roleData', function (req, res, next) {
     var roleInfo = req.body.roleInfo;
     var roleName = req.body.roleName;
+    var id = req.body.id;
     if (!roleInfo || !roleName) {
         return;
     }
+    let sql;
+    let param = [];
+    let createAt = Date.parse(new Date()) / 1000;
+    if(!id){
+        sql = r_role.insert
+        param = [roleInfo, roleName, createAt]
+    }else{
+        sql = r_role.updateRole
+        param = [roleInfo, roleName, id]
+    }
     pool.getConnection(function (err, connection) {
-        connection.query(r_user.insert, [roleInfo, roleName], function (err, result) {
+        
+        connection.query(sql, param, function (err, result) {
             if (result) {
                 result = {
                     code: 200,
                     msg: result
                 };
-            }else{
+            } else {
                 result = {
                     code: 200,
                     msg: err
@@ -42,9 +54,57 @@ router.post('/authorData', function (req, res, next) {
     })
 });
 
-router.get('/authorData', function (req, res, next) {
+router.post('/userData', function (req, res, next) {
+    var roleId = req.body.roleId;
+    var userName = req.body.userName;
+    var password = req.body.password;
+    var id = req.body.id;
+    if (!userName || !password) {
+        return;
+    }
+    let sql;
+    let param = [];
+    let createAt = Date.parse(new Date()) / 1000;
+    if (!id) {
+        sql = r_user.insert
+        param = [userName, password, roleId, createAt]
+    } else {
+        sql = r_user.updateRole
+        param = [userName, password, roleId ,id]
+    }
     pool.getConnection(function (err, connection) {
-        connection.query(r_user.queryAll, [], function (err, result) {
+        connection.query(sql, param, function (err, result) {
+            if (result) {
+                result = {
+                    code: 200,
+                    msg: result
+                };
+            } else {
+                result = {
+                    code: 200,
+                    msg: err
+                };
+            }
+            // 以json形式，把操作结果返回给前台页面
+            // 释放连接  
+            res.json(result)
+            connection.release();
+        });
+    })
+});
+
+
+
+router.get('/roleData', function (req, res, next) {
+    let id = req.query.id;
+    let sql;
+    if (id) {
+        sql = r_role.getRolePointsById
+    } else {
+        sql = r_role.queryAll
+    }
+    pool.getConnection(function (err, connection) {
+        connection.query(sql, [id], function (err, result) {
             if (result) {
                 result = {
                     code: 200,
@@ -59,6 +119,64 @@ router.get('/authorData', function (req, res, next) {
     })
 });
 
+router.get('/userData', function (req, res, next) {
+    let id = req.query.id;
+    let sql;
+    if (id) {
+        sql = r_user.getUserById
+    } else {
+        sql = r_user.queryAll
+    }
+    pool.getConnection(function (err, connection) {
+        connection.query(sql, [id], function (err, result) {
+            if (result) {
+                result = {
+                    code: 200,
+                    msg: result
+                };
+            }
+            // 以json形式，把操作结果返回给前台页面
+            // 释放连接  
+            res.json(result)
+            connection.release();
+        });
+    })
+});
+router.delete('/roleData', function (req, res, next) {
+    let id = req.query.id;
+    pool.getConnection(function (err, connection) {
+        connection.query(r_role.deleteRole, [id], function (err, result) {
+            if (result) {
+                result = {
+                    code: 200,
+                    msg: result
+                };
+            }
+            // 以json形式，把操作结果返回给前台页面
+            // 释放连接  
+            res.json(result)
+            connection.release();
+        });
+    })
+});
+
+router.delete('/userData', function (req, res, next) {
+    let id = req.query.id;
+    pool.getConnection(function (err, connection) {
+        connection.query(r_user.deleteRole, [id], function (err, result) {
+            if (result) {
+                result = {
+                    code: 200,
+                    msg: result
+                };
+            }
+            // 以json形式，把操作结果返回给前台页面
+            // 释放连接  
+            res.json(result)
+            connection.release();
+        });
+    })
+});
 
 
 module.exports = router
--- node/sql/r_user.js
+++ node/sql/r_user.js
@@ -1,5 +1,16 @@
-var r_user = {
-  insert: 'INSERT INTO `ycydata`.`r_role` (`roleInfo`, `roleName`) VALUES (?, ?);',
-  queryAll: 'SELECT * FROM r_role ORDER BY id',
+var r_role = {
+  insert: 'INSERT INTO `ycydata`.`r_role` (`roleInfo`, `roleName`, `createdAt`) VALUES (?, ?, ?);',
+  queryAll: 'SELECT * FROM r_role ORDER BY createdAt DESC',
+  getRolePointsById: 'SELECT * FROM r_role WHERE id = ? ORDER BY createdAt DESC LIMIT 1',
+  updateRole: 'UPDATE `ycydata`.`r_role` SET `roleInfo`=?, `roleName`=? WHERE `id`=?',
+  deleteRole: 'DELETE FROM `ycydata`.`r_role` WHERE `id`=?'
 };
-module.exports = r_user;
+var r_user = {
+  insert: 'INSERT INTO `ycydata`.`r_user` (`userName`, `password`, `role`, `createdAt`) VALUES (?, ?, ?, ?);',
+  // queryAll: 'SELECT * FROM r_user ORDER BY createdAt DESC',
+  queryAll: 'SELECT r_user.*,r_role.roleName FROM r_user INNER JOIN r_role ON r_user.role = r_role.id',
+  getUserById: 'SELECT r_user.*,r_role.roleName FROM r_user INNER JOIN r_role ON r_user.role = r_role.id WHERE r_user.id = ? ORDER BY createdAt DESC LIMIT 1',
+  updateRole: 'UPDATE `ycydata`.`r_user` SET `userName`=?, `password`=?, `role`=? WHERE `id`=?',
+  deleteRole: 'DELETE FROM `ycydata`.`r_user` WHERE `id`=?'
+};
+module.exports = { r_user, r_role };
